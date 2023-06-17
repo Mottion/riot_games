aac3d90e5804ef30a5af7b4f9085ab0889d8ca9c
Adrian Elizandro <adrianelizandro78@gmail.com>
2023-06-17 12:17:21 -0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <Header>
         <img
-          src="headerBackgroundImg.png" 
+          src="/headerBackgroundImg.png" 
           alt="headerbackgroundImg" 
           className='headerbackgroundImg'
         />
